76b475f7aabe5ff797d24992537357d9c322e3bd
aditya22oct <aditya22oct@gmail.com>
2021-10-07 23:21:02 +0200

    Created find gap in wall function

--- src/laser_data_subscriber.py
+++ src/laser_data_subscriber.py
@@ -3,9 +3,31 @@
 import rospy
 from sensor_msgs.msg import LaserScan
 
-def LaserScan_cb(msg):
-    print(len(msg.ranges))
+regions = None
 
-rospy.init_node('laser_subscriber')
-rospy.Subscriber('/scan', LaserScan, LaserScan_cb)
-rospy.spin()
+def LaserScan_cb(msg):
+
+    global regions
+    laser_data = msg.ranges
+    regions = [
+    laser_data[0:29],
+    laser_data[330:359],
+    laser_data[270:299],
+    laser_data[240:269],
+    ]
+
+
+def find_gap():
+    global regions
+    print(regions)
+
+
+def main():
+    rospy.init_node('laser_subscriber')
+    rospy.Subscriber('/scan', LaserScan, LaserScan_cb)
+    find_gap()
+    rospy.spin()
+
+
+if __name__ == '__main__':
+    main()
