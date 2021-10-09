0e56546855339961b5dadfc2e76e3975c2abf035
aditya22oct <aditya22oct@gmail.com>
2021-10-09 14:38:16 +0200

    Added take_action function

--- src/go_to_point_w-o_obstacle.py
+++ src/go_to_point_w-o_obstacle.py
@@ -1,4 +1,4 @@
-#! usr/bin/#!/usr/bin/env python3
+#!/usr/bin/env python3
 
 import rospy
 from geometry_msgs.msg import Twist, Point
@@ -12,3 +12,48 @@ g_desired_pos = Point()
 g_desired_pos.x = 5.0
 g_desired_pos.y = 0.0
 g_desired_pos.z = 0.0
+
+
+g_kill_node = False
+g_bot_state= 0
+g_pose_acc = 0.01
+
+def odmetry_cb(msg):
+
+    curr_pose = msg.pose.pose.position
+    take_action(curr_pose)
+
+
+def take_action(pose_data):
+
+    pub_cmd_vel = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
+    vel_value = Twist()
+
+    x_diff = g_desired_pos.x - pose_data.x
+    y_diff = g_desired_pos.y - pose_data.y
+
+    distance = math.sqrt(math.pow(x_diff, 2)+math.pow(y_diff, 2))
+
+    if distance >= g_pose_acc:
+        vel_value.linear.x = 0.2
+    else:
+        vel_value.linear.x = 0.0
+
+    pub_cmd_vel.publish(vel_value)
+    rospy.loginfo(distance)
+
+
+def main():
+
+    global pub_cmd_vel, vel_value, kill_node
+
+    rospy.init_node('turn_robot')
+    r = rospy.Rate(10)
+
+    sub_odom = rospy.Subscriber('/odom', Odometry, odmetry_cb)
+
+    rospy.spin()
+
+
+if __name__ == '__main__':
+    main()
