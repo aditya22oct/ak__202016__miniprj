74258c8c58edf034edc268979fb54f42237612a3
aditya22oct <aditya22oct@gmail.com>
2021-10-11 23:33:26 +0200

    Editing laser subscriber for wall following algorithm

--- src/laser_data_subscriber.py
+++ src/laser_data_subscriber.py
@@ -5,17 +5,24 @@ from sensor_msgs.msg import LaserScan
 
 regions = None
 
+g_regions = {}
 def LaserScan_cb(msg):
 
-    global regions
-    laser_data = msg.ranges
-    regions = [
-    laser_data[0:29],
-    laser_data[330:359],
-    laser_data[270:299],
-    laser_data[240:269],
-    ]
-
+    global g_regions
+    laser_data = list(msg.ranges)
+    front_region = laser_data[0:18] + laser_data[342:]
+    left_front_region = laser_data[18:54]
+    right_front_region = laser_data[306:342]
+    print(len(laser_data))
+    print(len(front_region))
+    print(len(left_front_region))
+    print(len(right_front_region ))
+    print(type(laser_data))
+    g_regions = {'front_region': min([min(front_region),10]),
+    'left_front_region': min([min(left_front_region),10]),
+    'right_front_region': min([min(right_front_region),10])
+    }
+    print(g_regions)
 
 def find_gap():
     global regions
@@ -25,7 +32,6 @@ def find_gap():
 def main():
     rospy.init_node('laser_subscriber')
     rospy.Subscriber('/scan', LaserScan, LaserScan_cb)
-    find_gap()
     rospy.spin()
 
 
